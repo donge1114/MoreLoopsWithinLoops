dadc5c25e3f6aa6d1d7a88284bdc525ee074d937
donge1114 <donge@rose-hulman.edu>
2018-02-15 15:08:15 -0500

    DONE

--- src/m3_more_nested_loops_in_sequences.py
+++ src/m3_more_nested_loops_in_sequences.py
@@ -3,21 +3,21 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of SEQUENCES OF SUB-SEQUENCES.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Enyi Dong.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
     """ Calls the other functions to test them. """
-    run_test_largest_number()
-    run_test_largest_negative_number()
+    # run_test_largest_number()
+    # run_test_largest_negative_number()
     run_test_first_is_elsewhere_too()
 
 
 def run_test_largest_number():
     """ Tests the    largest_number    function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  largest_number  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond those we wrote.
     # ------------------------------------------------------------------
@@ -44,6 +44,13 @@ def run_test_largest_number():
     print('Expected and actual are:', expected, answer)
 
     # TO DO 2 (continued): Add your ADDITIONAL test(s) here:
+
+    # test 4:
+    expected = 8
+    answer = largest_number([(1, 2, 3),
+                             (4, 5),
+                             [6, 7, 8]])
+    print('Expected and actual are:', expected, answer)
 
 
 def largest_number(seq_seq):
@@ -72,15 +79,37 @@ def largest_number(seq_seq):
     where each subsequence contains only numbers.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
+    # for x in range(len(seq_seq)):
+    #     if len(seq_seq[x]):
+    #         max = seq_seq[x][0]
+    #
+    # for i in range(len(seq_seq)):
+    #     for k in range(len(seq_seq[i])):
+    #         if seq_seq[i][k] > max:
+    #             max = seq_seq[i][k]
+    # return max
+
+    list = []
+    for i in range(len(seq_seq)):
+        for j in range(len(seq_seq[i])):
+            if len(seq_seq[i]):
+                list +=[seq_seq[i][j]]
+    if not list:
+        return None
+    max = list[0]
+    for k in range(len(list)):
+        if max < list[k]:
+            max = list[k]
+    return max
 
 
 def run_test_largest_negative_number():
     """ Tests the    largest_negative_number    function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this TEST function.
+    # DONE: 4. Implement this TEST function.
     #   It TESTS the  largest_negative_number  function defined below.
     #
     #   Include enough tests to give you confidence that your solution
@@ -90,7 +119,22 @@ def run_test_largest_negative_number():
     print('-------------------------------------------------')
     print('Testing the   LARGEST_NEGATIVE_NUMBER   function:')
     print('-------------------------------------------------')
+    # Test 1:
+    expected = -1
+    answer = largest_negative_number([(-13, 4),
+                             (33, -10, 11, -7, 10),
+                             (-1, 2, -31, -4)])
+    print('Expected and actual are:', expected, answer)
 
+    # Test 2:
+    expected = -111111
+    answer = largest_negative_number(([], [-111111], []))
+    print('Expected and actual are:', expected, answer)
+
+    # Test 3:
+    expected = None
+    answer = largest_negative_number(([], [], []))
+    print('Expected and actual are:', expected, answer)
 
 def largest_negative_number(seq_seq):
     """
@@ -115,14 +159,26 @@ def largest_negative_number(seq_seq):
     where each subsequence contains only numbers.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     # CHALLENGE: Try to solve this problem with no additional sequences
     #   being constructed (so the SPACE allowed is limited to the
     #   give sequence of sequences plus any non-list variables you want).
     # ------------------------------------------------------------------
+    list = []
+    for i in range(len(seq_seq)):
+        for j in range(len(seq_seq[i])):
+            if len(seq_seq[i]) and seq_seq[i][j] < 0:
+                list += [seq_seq[i][j]]
+    if not list:
+        return None
 
+    max = list[0]
+    for k in range(len(list)):
+        if max < list[k]:
+            max = list[k]
+    return max
 
 def run_test_first_is_elsewhere_too():
     """ Tests the    first_is_elsewhere_too    function. """
@@ -355,7 +411,7 @@ def first_is_elsewhere_too(seq_seq):
     and the given argument is a sequence of sequences.
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -371,6 +427,16 @@ def first_is_elsewhere_too(seq_seq):
     #   practice at loops within loops (within loops within ...)
     # ------------------------------------------------------------------
 
+    list = []
+    for i in range(1, len(seq_seq)):
+        for j in range(len(seq_seq[i])):
+            list +=[seq_seq[i][j]]
+    for x in range(len(seq_seq[0])):
+        a = seq_seq[0][x]
+        for y in range(len(list)):
+            if a == list[y]:
+                return True
+    return False
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
